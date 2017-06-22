3e0a97f56c686ac858927f1e392d0c63339250db
Simon Hitzemann <github@simon.hitzemann.org>
2017-06-22 11:06:30 +0200

    Basic backend skeleton done?

---
+++ mopidy_sidplay/SidplayBackend.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import logging
+import os
+import threading
+
+from mopidy import backend
+
+import pykka
+
+logger = logging.getLogger(__name__)
+
+class SidplayBackend(pykka.ThreadingActor, backend.Backend):
+    def __init__(self, config, audio):
+        super(SidplayBackend, self).__init__()
+        self.config = config
